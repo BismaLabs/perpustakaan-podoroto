24e0af71e96b029d361a77aa7354871c27110f6d
maulayyacyber <maulayyacyber@gmail.com>
2017-04-06 09:40:52 +0700

    add fungsi date time

--- app/models/Apps.php
+++ app/models/Apps.php
@@ -132,4 +132,238 @@ class Apps extends CI_Model{
         $id_user  =  array('id_user'=> $id_user);
         return $this->db->get_where('tbl_users', $id_user);
     }
+
+    //fungsi date time
+        function tgl_time_indo($date=null){
+            $tglindo = date("d-m-Y H:i:s", strtotime($date));
+            $formatTanggal = $tglindo;
+            return $formatTanggal;
+        }
+
+        function tgl_database($date=null)
+        {
+            $tgldatabase = date("Y-m-d", strtotime($date));
+            $formatTanggal = $tgldatabase;
+            return $formatTanggal;
+        }
+
+        function tgl_indo($date=null)
+        {
+            $tglindo = date("d-m-Y", strtotime($date));
+            $formatTanggal = $tglindo;
+            return $formatTanggal;
+        }
+
+        function tgl_tunggal($date=null)
+        {
+            $tglindo = date("j", strtotime($date));
+            $formatTanggal = $tglindo;
+            return $formatTanggal;
+        }
+
+        function tgl_mont_year($date=null)
+        {
+            $tglindo = date("n, Y");
+            return $tglindo;
+        }
+
+        function year_tunggal($date=null)
+        {
+            $tglindo = date("Y");
+            return $tglindo;
+        }
+
+        function jam_format($time=null)
+        {
+            $jamformat = date("H:i", strtotime($time));
+            $formatJam = $jamformat;
+            return $formatJam;
+        }
+
+        function bulan_inggris($date=null){
+            //buat array nama hari dalam bahasa Indonesia dengan urutan 1-7
+            $array_hari = array(1=>'Senin','Selasa','Rabu','Kamis','Jumat', 'Sabtu','Minggu');
+            //buat array nama bulan dalam bahasa Indonesia dengan urutan 1-12
+            $array_bulan = array(1=>'Jan','Feb','Mar', 'Apr', 'May', 'Jun','Jul','Aug',
+                'Sep','Oct', 'Nov','Dec');
+            if($date == null) {
+                //jika $date kosong, makan tanggal yang diformat adalah tanggal hari ini
+                $hari = $array_hari[date('N')];
+                $tanggal = date ('j');
+                $bulan = $array_bulan[date('n')];
+                $tahun = date('Y');
+            } else {
+                //jika $date diisi, makan tanggal yang diformat adalah tanggal tersebut
+                $date = strtotime($date);
+                $hari = $array_hari[date('N',$date)];
+                $tanggal = date ('j', $date);
+                $bulan = $array_bulan[date('n',$date)];
+                $tahun = date('Y',$date);
+            }
+            $formatTanggal = $bulan;
+            return $formatTanggal;
+        }
+
+        function tgl_indo_no_hari($date=null){
+            //buat array nama hari dalam bahasa Indonesia dengan urutan 1-7
+            $array_hari = array(1=>'Senin','Selasa','Rabu','Kamis','Jumat', 'Sabtu','Minggu');
+            //buat array nama bulan dalam bahasa Indonesia dengan urutan 1-12
+            $array_bulan = array(1=>'Januari','Februari','Maret', 'April', 'Mei', 'Juni','Juli','Agustus',
+                'September','Oktober', 'November','Desember');
+            if($date == null) {
+                //jika $date kosong, makan tanggal yang diformat adalah tanggal hari ini
+                $hari = $array_hari[date('N')];
+                $tanggal = date ('j');
+                $bulan = $array_bulan[date('n')];
+                $tahun = date('Y');
+            } else {
+                //jika $date diisi, makan tanggal yang diformat adalah tanggal tersebut
+                $date = strtotime($date);
+                $hari = $array_hari[date('N',$date)];
+                $tanggal = date ('j', $date);
+                $bulan = $array_bulan[date('n',$date)];
+                $tahun = date('Y',$date);
+            }
+            $formatTanggal = $tanggal ." ". $bulan ." ". $tahun;
+            return $formatTanggal;
+        }
+
+        function bulan_indo($date=null){
+            //buat array nama hari dalam bahasa Indonesia dengan urutan 1-7
+            $array_hari = array(1=>'Senin','Selasa','Rabu','Kamis','Jumat', 'Sabtu','Minggu');
+            //buat array nama bulan dalam bahasa Indonesia dengan urutan 1-12
+            $array_bulan = array(1=>'Januari','Februari','Maret', 'April', 'Mei', 'Juni','Juli','Agustus',
+                'September','Oktober', 'November','Desember');
+            if($date == null) {
+                //jika $date kosong, makan tanggal yang diformat adalah tanggal hari ini
+                $hari = $array_hari[date('N')];
+                $tanggal = date ('j');
+                $bulan = $array_bulan[date('n')];
+                $tahun = date('Y');
+            } else {
+                //jika $date diisi, makan tanggal yang diformat adalah tanggal tersebut
+                $date = strtotime($date);
+                $hari = $array_hari[date('N',$date)];
+                $tanggal = date ('j', $date);
+                $bulan = $array_bulan[date('n',$date)];
+                $tahun = date('Y',$date);
+            }
+            $formatTanggal = $bulan;
+            return $formatTanggal;
+        }
+
+        function tgl_indo_lengkap($date=null){
+            //buat array nama hari dalam bahasa Indonesia dengan urutan 1-7
+            $array_hari = array(1=>'Senin','Selasa','Rabu','Kamis','Jumat', 'Sabtu','Minggu');
+            //buat array nama bulan dalam bahasa Indonesia dengan urutan 1-12
+            $array_bulan = array(1=>'Januari','Februari','Maret', 'April', 'Mei', 'Juni','Juli','Agustus',
+                'September','Oktober', 'November','Desember');
+            if($date == null) {
+                //jika $date kosong, makan tanggal yang diformat adalah tanggal hari ini
+                $hari = $array_hari[date('N')];
+                $tanggal = date ('d');
+                $bulan = $array_bulan[date('n')];
+                $tahun = date('Y');
+                $jam = date('H:i:s');
+            } else {
+                //jika $date diisi, makan tanggal yang diformat adalah tanggal tersebut
+                $date = strtotime($date);
+                $hari = $array_hari[date('N',$date)];
+                $tanggal = date ('d', $date);
+                $bulan = $array_bulan[date('n',$date)];
+                $tahun = date('Y',$date);
+                $jam = date('H:i:s',$date);
+            }
+            $formatTanggal = $hari . ", " . $tanggal ." ". $bulan ." ". $tahun ." Jam ". $jam;
+            return $formatTanggal;
+        }
+
+        function tgl_jam_indo_no_hari($date=null){
+            //buat array nama hari dalam bahasa Indonesia dengan urutan 1-7
+            $array_hari = array(1=>'Senin','Selasa','Rabu','Kamis','Jumat', 'Sabtu','Minggu');
+            //buat array nama bulan dalam bahasa Indonesia dengan urutan 1-12
+            $array_bulan = array(1=>'Januari','Februari','Maret', 'April', 'Mei', 'Juni','Juli','Agustus',
+                'September','Oktober', 'November','Desember');
+            if($date == null) {
+                //jika $date kosong, makan tanggal yang diformat adalah tanggal hari ini
+                $hari = $array_hari[date('N')];
+                $tanggal = date ('d');
+                $bulan = $array_bulan[date('n')];
+                $tahun = date('Y');
+                $jam = date('H:i:s');
+            } else {
+                //jika $date diisi, makan tanggal yang diformat adalah tanggal tersebut
+                $date = strtotime($date);
+                $hari = $array_hari[date('N',$date)];
+                $tanggal = date ('d', $date);
+                $bulan = $array_bulan[date('n',$date)];
+                $tahun = date('Y',$date);
+                $jam = date('H:i:s',$date);
+            }
+            $formatTanggal = $tanggal ." ". $bulan ." ". $tahun ." Jam ". $jam;
+            return $formatTanggal;
+        }
+
+        //fungsi date ago
+        function time_elapsed_string($datetime, $full = false) {
+            $today = time();
+            $createdday= strtotime($datetime);
+            $datediff = abs($today - $createdday);
+            $difftext="";
+            $years = floor($datediff / (365*60*60*24));
+            $months = floor(($datediff - $years * 365*60*60*24) / (30*60*60*24));
+            $days = floor(($datediff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
+            $hours= floor($datediff/3600);
+            $minutes= floor($datediff/60);
+            $seconds= floor($datediff);
+            //year checker
+            if($difftext=="")
+            {
+                if($years>1)
+                    $difftext=$years." years ago";
+                elseif($years==1)
+                    $difftext=$years." year ago";
+            }
+            //month checker
+            if($difftext=="")
+            {
+                if($months>1)
+                    $difftext=$months." months ago";
+                elseif($months==1)
+                    $difftext=$months." month ago";
+            }
+            //month checker
+            if($difftext=="")
+            {
+                if($days>1)
+                    $difftext=$days." days ago";
+                elseif($days==1)
+                    $difftext=$days." day ago";
+            }
+            //hour checker
+            if($difftext=="")
+            {
+                if($hours>1)
+                    $difftext=$hours." hours ago";
+                elseif($hours==1)
+                    $difftext=$hours." hour ago";
+            }
+            //minutes checker
+            if($difftext=="")
+            {
+                if($minutes>1)
+                    $difftext=$minutes." minutes ago";
+                elseif($minutes==1)
+                    $difftext=$minutes." minute ago";
+            }
+            //seconds checker
+            if($difftext=="")
+            {
+                if($seconds>1)
+                    $difftext=$seconds." seconds ago";
+                elseif($seconds==1)
+                    $difftext=$seconds." second ago";
+            }
+            return $difftext;
+        }
 }
