42afed8c35d233b065e3124dbc9bab08aedc7447
maulayyacyber <maulayyacyber@gmail.com>
2017-04-06 09:51:18 +0700

    fix

--- resources/backend/dist/js/visitor.js
+++ resources/backend/dist/js/visitor.js
@@ -1,5 +1,5 @@
 // deklarasi path url host
-var url = window.location.origin+"/~maulayyacyber/perpustakaan-podoroto/apps/";
+var url = "htpp://localhost/~maulayyacyber/perpustakaan-podoroto/apps/";
 
 // Get data chart
 function GetToday(tgl)
