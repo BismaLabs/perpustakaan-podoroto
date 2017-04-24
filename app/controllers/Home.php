7ff073d0a83f93f7332a7cbd323140a87fc37cd7
maulayyacyber <maulayyacyber@gmail.com>
2017-04-24 14:26:25 +0700

    fix bug

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,52 +3,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
-
-<<<<<<< HEAD
     public function __construct()
     {
         parent::__construct();
         //panggil model
-        $this->load->model('apps');
+        $this->load->model(array('apps', 'web'));
     }
 
     public function index()
     {
-        if ($this->apps->apps_id()) {
-
-            $data = array(
-                'title'         => 'Perpustakaan Desa Podoroto, Kesamben, Jombang, Jawa Timur',
-                'data_buku'     => $this->apps->select_buku(),
-                'data_kategori' => $this->apps->select_kategori(),
-                'data_page'     => $this->apps->select_pages()
-            );
-            $this->load->view('public/part/header', $data);
-            $this->load->view('public/part/slider');
-            $this->load->view('public/layout/home/data');
-            $this->load->view('public/part/footer');
-        } else {
-            show_404();
-            return FALSE;
-        }
-    }
-=======
-	public function __construct()
-	{
-		parent::__construct();
-		//panggil model
-		$this->load->model(array('apps','web'));
-	}
-
-	public function index()
-	{
-			$data = array('title' => 'Perpustakaan Desa Podoroto, Kesamben, Jombang, Jawa Timur',
-			'data_buku'=> $this->web->select_buku(),
-			'data_kategori'=> $this->web->select_kategori()
-			);
-		$this->load->view('public/part/header', $data);
+        $data = array(
+            'title'         => 'Perpustakaan Desa Podoroto, Kesamben, Jombang, Jawa Timur',
+            'data_buku'     => $this->web->select_buku(),
+            'data_kategori' => $this->web->select_kategori()
+        );
+        $this->load->view('public/part/header', $data);
         $this->load->view('public/part/slider');
         $this->load->view('public/layout/home/data');
         $this->load->view('public/part/footer');
-	}
->>>>>>> 5c721c83279578fcd39f4909aeff21784ba09737
+    }
+
 }
