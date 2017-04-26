bf258a9ab38a6e7d84776c450a869d3b47497bdd
myazidinniam <myazidinniam@gmail.com>
2017-04-26 16:16:02 +0700

    layout kategori

--- app/controllers/Kategori.php
+++ app/controllers/Kategori.php
@@ -35,8 +35,18 @@ Class Kategori extends CI_Controller {
 			'keywords'      => systems('keywords'),
             'descriptions'  => systems('descriptions'),
 			);
+
+		$query = $this->db->query("SELECT a.kode_buku, a.judul_buku, a.foto, a.slug, a.tahun_terbit, a.penerbit, a.pengarang, a.kategori_id, a.deskripsi, a.no_isbn, a.jumlah_buku, a.created_at, b.id_kategori, b.nama_kategori FROM tbl_buku as a JOIN tbl_kategori as b ON a.kategori_id = b.id_kategori WHERE a.slug = '$url'");
+         if($query->num_rows() > 0)
+        {
+            return $query->row();
+        }else
+        {
+            return NULL;
+        }
+
 		$this->load->view('public/part/header', $data);
-        $this->load->view('public/layout/kategori/data');
+        $this->load->view('public/layout/kategori/detail');
         $this->load->view('public/part/footer');
 	}
 }
